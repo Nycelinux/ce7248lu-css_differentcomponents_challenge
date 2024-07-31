b37f1debd3bc9d19d515df9a7d0b3b645c6524e9
Nycelinux <celina.ludwigs@fh-erfurt.de>
2024-07-31 21:19:27 +0200

    order structure changed, 3D Cards carousel added

---
+++ cards/3DCarousel/3dCarousel.js
@@ -0,0 +1,91 @@
+let radius = 240;
+const autoRotate = true;
+const rotateSpeed = -60;
+const imgWidth = 120;
+const imgHeight = 170;
+let tY = 10;
+let tX = 0;
+let desY = 0;
+let sX, sY, nX, nY, desX = 0;
+
+setTimeout(init, 1000);
+
+const odrag = document.getElementById('dragContainer');
+const ospin = document.getElementById('spinContainer');
+const aImg = ospin.getElementsByTagName('img');
+const aEle = [...aImg];
+
+ospin.style.width = imgWidth + "px";
+ospin.style.height = imgHeight + "px";
+
+const ground = document.getElementById('ground');
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
+
+function init(delayTime) {
+    for (let i = 0; i < aEle.length; i++) {
+        aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+        aEle[i].style.transition = "transform 1s";
+        aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+    }
+}
+
+function applyTransition(obj) {
+    if (tY > 180) tY = 180;
+    if (tY < 0) tY = 0;
+    obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+}
+
+function playSpin(yes) {
+    ospin.style.animationPlayState = (yes ? 'running' : 'paused');
+}
+
+if (autoRotate) {
+    let animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+    ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+}
+
+document.onpointerdown = function (e) {
+    clearInterval(odrag.timer);
+    e = e || window.event;
+    sX = e.clientX;
+    sY = e.clientY;
+
+    this.onpointermove = function (e) {
+        e = e || window.event;
+        nX = e.clientX;
+        nY = e.clientY;
+        desX = nX - sX;
+        desY = nY - sY;
+        tX += desX * 0.1;
+        tY += desY * 0.1;
+        applyTransition(odrag);
+        sX = nX;
+        sY = nY;
+    };
+
+    this.onpointerup = function (e) {
+        odrag.timer = setInterval(function () {
+            desX *= 0.95;
+            desY *= 0.95;
+            tX += desX * 0.1;
+            tY += desY * 0.1;
+            applyTransition(odrag);
+            playSpin(false);
+
+            if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+                clearInterval(odrag.timer);
+                playSpin(true);
+            }
+        }, 17);
+        this.onpointermove = this.onpointerup = null;
+    }
+    return false;
+};
+
+document.onmousewheel = function (e) {
+    e = e || window.event;
+    let d = e.wheelDelta / 20 || -e.detail;
+    radius += d;
+    init(1);
+};
